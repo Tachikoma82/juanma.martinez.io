d8393101ecb0a38286ab3ca60de08b62d335122f
Tachikoma82 <losdomingosdejuano@gmail.com>
2019-10-22 21:29:40 +0200

    Versión beta - base sobre la que empezar a trabajar_2

--- src/main.js
+++ src/main.js
@@ -2,11 +2,11 @@ document.querySelector('.menu__btn').addEventListener('click', ()=>{
   const header= document.querySelector('.header')
   const headerMenu= document.querySelector('.header__menu')
 
-  // if(header.classList.contains('header--desplegado')){
-  //   header.classList.remove('header--desplegado')
-  // }else{
-  //   header.classList.add('header--desplegado')
-  // }
+   if(header.classList.contains('header--desplegado')){
+     header.classList.remove('header--desplegado')
+   }else{
+     header.classList.add('header--desplegado')
+   }
   header.classList.toggle('header--desplegado')
   headerMenu.classList.toggle('header__menu--opacity')
 })
